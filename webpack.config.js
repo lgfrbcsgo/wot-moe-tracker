895f8584f0ad5018bdf3663c3f16da628dbab1a4
lgfrbcsgo <33672090+lgfrbcsgo@users.noreply.github.com>
2021-05-30 16:44:25 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer")
+const TerserJSPlugin = require("terser-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin")
+const path = require("path")
+
+module.exports = ({ ifDev, ifProd }) => ({
+    ...ifDev({
+        mode: "development",
+        devtool: "eval-source-map",
+    }),
+    ...ifProd({
+        mode: "production",
+        devtool: "source-map",
+    }),
+    output: {
+        path: path.join(__dirname, "./dist"),
+        filename: "[name].[hash].js",
+        ...ifProd({
+            publicPath: "/wot-moe-tracker/",
+        }),
+    },
+    devServer: {
+        publicPath: "/",
+        contentBase: "./dist",
+        port: 8080,
+        historyApiFallback: true,
+    },
+    entry: "./src/main.ts",
+    resolve: {
+        extensions: [".ts", ".js"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: "ts-loader",
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules: {
+                                exportLocalsConvention: "camelCase",
+                            },
+                        },
+                    },
+                    "sass-loader",
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: "./public/index.html",
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].[hash].css",
+        }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static",
+            openAnalyzer: false,
+        }),
+    ],
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /node_modules/,
+                    chunks: "initial",
+                    name: "vendor",
+                },
+            },
+        },
+        minimizer: ifProd([new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})]),
+    },
+})
