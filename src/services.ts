7e930588d7e231ed9ca5fad01cf71b568bb8edc4
lgfrbcsgo <33672090+lgfrbcsgo@users.noreply.github.com>
2021-05-30 18:11:18 +0200

    Add Depends helper type

--- src/services.ts
+++ src/services.ts
@@ -1,2 +1,14 @@
 export const services = {}
+
 export type Services = typeof services
+
+export type Depends<Service> = Pick<
+    Services,
+    keyof Services extends infer Key
+        ? Key extends keyof Services
+            ? Services[Key] extends Service
+                ? Key
+                : never
+            : never
+        : never
+>
