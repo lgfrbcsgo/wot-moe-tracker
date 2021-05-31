a09e5b02ae452c905e3037f09f4cdd3039490b7a
lgfrbcsgo <33672090+lgfrbcsgo@users.noreply.github.com>
2021-05-31 22:20:11 +0200

    Add command for connecting to the MoE server

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,67 @@
-import { Init, Update, View } from "./runtime"
-import { impl, matchExhaustive, Variant } from "@practical-fp/union-types"
+import { Command, Init, Update, View } from "./runtime"
+import { impl, Variant } from "@practical-fp/union-types"
 import { html } from "lit-html"
+import {
+    isMarkOfExcellenceHistory,
+    isMarkOfExcellenceUpdate,
+    MarkOfExcellenceHistory,
+    MarkOfExcellenceUpdate,
+} from "./serverMessages"
+import { isString } from "./guards"
 
-export type Msg = Variant<"Increment"> | Variant<"Decrement">
-export const { Increment, Decrement } = impl<Msg>()
+export type Msg =
+    | Variant<"Connecting">
+    | Variant<"Connected">
+    | Variant<"UpdateReceived", MarkOfExcellenceUpdate>
+    | Variant<"HistoryReceived", MarkOfExcellenceHistory>
+    | Variant<"MessageDecodeFailure">
+    | Variant<"ConnectionError">
+    | Variant<"Closed">
 
-export type State = number
+export const {
+    Connecting,
+    Connected,
+    UpdateReceived,
+    HistoryReceived,
+    MessageDecodeFailure,
+    ConnectionError,
+    Closed,
+} = impl<Msg>()
 
-export const init: Init<State, Msg> = () => [0]
+const connect: Command<Msg> = dispatch => {
+    const socket = new WebSocket("ws://localhost:15456")
+    socket.onopen = () => dispatch(Connected())
+    socket.onmessage = event => {
+        if (!isString(event.data)) {
+            return dispatch(MessageDecodeFailure())
+        }
+        let payload: unknown
+        try {
+            payload = JSON.parse(event.data)
+        } catch (e) {
+            return dispatch(MessageDecodeFailure())
+        }
+        if (isMarkOfExcellenceUpdate(payload)) {
+            return dispatch(UpdateReceived(payload))
+        } else if (isMarkOfExcellenceHistory(payload)) {
+            return dispatch(HistoryReceived(payload))
+        } else {
+            return dispatch(MessageDecodeFailure())
+        }
+    }
+    socket.onerror = () => dispatch(ConnectionError())
+    socket.onclose = () => dispatch(Closed())
 
-export const update: Update<State, Msg> = (state, message) =>
-    matchExhaustive(message, {
-        Increment: () => [state + 1] as const,
-        Decrement: () => [state - 1] as const,
-    })
+    dispatch(Connecting())
+}
 
-export const view: View<State, Msg> = (state, dispatch) => html`
-    <h1>${state}</h1>
-    <button @click=${() => dispatch(Increment())}>+</button>
-    <button @click=${() => dispatch(Decrement())}>-</button>
-`
+export type State = undefined
+
+export const init: Init<State, Msg> = () => [undefined, connect]
+
+export const update: Update<State, Msg> = (state, message) => [
+    undefined,
+    () => console.log(message),
+]
+
+export const view: View<State, Msg> = (state, dispatch) => html` <h1>Works!</h1> `
