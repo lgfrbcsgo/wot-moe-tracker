66b53efa378e4cf0df909ecadf55b263fe3535b0
lgfrbcsgo <33672090+lgfrbcsgo@users.noreply.github.com>
2021-06-05 09:54:54 +0200

    Add database handling

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,32 @@
 import { Init, Update, View } from "./runtime"
 import { html } from "lit-html"
 import { Connection, ConnectionMessage } from "./connection"
+import { Database, DatabaseStatus } from "./database"
+import { impl, Variant } from "@practical-fp/union-types"
 
-export type Msg = ConnectionMessage
+export type Message =
+    | Variant<"ConnectionMessage", ConnectionMessage>
+    | Variant<"DatabaseStatusChange", DatabaseStatus>
+
+const { ConnectionMessage, DatabaseStatusChange } = impl<Message>()
 
 const connection = new Connection()
 connection.connect()
 
+const database = new Database()
+database.open()
+
 export type State = undefined
 
-export const init: Init<State, Msg> = () => [undefined, dispatch => connection.observe(dispatch)]
+export const init: Init<State, Message> = () => [
+    undefined,
+    dispatch => connection.observe(message => dispatch(ConnectionMessage(message))),
+    dispatch => database.observe(status => dispatch(DatabaseStatusChange(status))),
+]
 
-export const update: Update<State, Msg> = (state, message) => [
+export const update: Update<State, Message> = (state, message) => [
     undefined,
     () => console.log(message),
 ]
 
-export const view: View<State, Msg> = (state, dispatch) => html` <h1>Works!</h1> `
+export const view: View<State, Message> = (state, dispatch) => html` <h1>Works!</h1> `
--- src/database.ts
+++ src/database.ts
@@ -1,4 +1,17 @@
 import { DBSchema, openDB } from "idb"
+import { impl, Variant } from "@practical-fp/union-types"
+import { Emitter, Observable } from "./emitter"
+import { IDBPDatabase } from "idb/build/esm/entry"
+
+export type DatabaseStatus =
+    | Variant<"Closed">
+    | Variant<"Opening">
+    | Variant<"Open">
+    | Variant<"Error">
+    | Variant<"Blocked">
+    | Variant<"Blocking">
+
+export const { Closed, Opening, Open, Error, Blocked, Blocking } = impl<DatabaseStatus>()
 
 export interface MarkOfExcellenceRecord {
     timestamp: number
@@ -23,13 +36,44 @@ interface Schema extends DBSchema {
     }
 }
 
-function connect() {
-    return openDB<Schema>("moeDatabase", 1, {
-        upgrade(database) {
-            const store = database.createObjectStore("moeStore", {
-                keyPath: ["account", "vehicle"],
+export class Database implements Observable<DatabaseStatus> {
+    private status: DatabaseStatus = Closed()
+    private database: IDBPDatabase<Schema> | undefined
+    private emitter = new Emitter<DatabaseStatus>()
+
+    open() {
+        if (Opening.is(this.status) || Open.is(this.status)) return
+        this.handleStatusChange(Opening())
+        openDB<Schema>("moeDatabase", 1, {
+            upgrade: database => {
+                const store = database.createObjectStore("moeStore", {
+                    keyPath: ["account", "vehicle"],
+                })
+                store.createIndex("account", "account")
+            },
+            blocked: () => this.handleStatusChange(Blocked()),
+            blocking: () => this.handleStatusChange(Blocking()),
+            terminated: () => {
+                this.handleStatusChange(Error())
+                this.database = undefined
+            },
+        })
+            .then(database => {
+                this.database = database
+                this.handleStatusChange(Open())
             })
-            store.createIndex("account", "account")
-        },
-    })
+            .catch(() => {
+                this.handleStatusChange(Error())
+            })
+    }
+
+    observe(observer: (value: DatabaseStatus) => void) {
+        observer(this.status)
+        return this.emitter.observe(observer)
+    }
+
+    handleStatusChange(newStatus: DatabaseStatus) {
+        this.status = newStatus
+        this.emitter.emit(newStatus)
+    }
 }
