42cc3334c97f1a88e6e71bf23c0a5f14877b95d5
lgfrbcsgo <33672090+lgfrbcsgo@users.noreply.github.com>
2021-06-06 11:35:51 +0200

    Write tank data to src/tanks.json

--- generate-tanks-json.js
+++ generate-tanks-json.js
@@ -1,10 +1,11 @@
+const fs = require("fs")
 const got = require("got")
 
 const API_KEY = process.env.WG_API_KEY
 
 function makeUrl(pageNumber) {
     const baseUrl = "https://api.worldoftanks.eu/wot/encyclopedia/vehicles/"
-    const fields = ["tank_id", "name", "short_name", "tier", "images"].join("%2C")
+    const fields = ["short_name", "tier", "images.contour_icon", "images.big_icon"].join("%2C")
     const tiers = [5, 6, 7, 8, 9, 10].join("%2C")
     return `${baseUrl}?application_id=${API_KEY}&fields=${fields}&tier=${tiers}&page_no=${pageNumber}`
 }
@@ -29,4 +30,4 @@ async function fetchAll() {
     return result
 }
 
-fetchAll().then(console.log)
+fetchAll().then(data => fs.writeFileSync("./src/tanks.json", JSON.stringify(data, null, 2)))
