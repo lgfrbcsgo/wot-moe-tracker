f75aed7a1ef3a8d4de684b0c8ae1ee6aa8801869
lgfrbcsgo <33672090+lgfrbcsgo@users.noreply.github.com>
2021-06-06 11:28:49 +0200

    WIP Add script for fetching tankopedia information

---
+++ generate-tanks-json.js
@@ -0,0 +1,32 @@
+const got = require("got")
+
+const API_KEY = process.env.WG_API_KEY
+
+function makeUrl(pageNumber) {
+    const baseUrl = "https://api.worldoftanks.eu/wot/encyclopedia/vehicles/"
+    const fields = ["tank_id", "name", "short_name", "tier", "images"].join("%2C")
+    const tiers = [5, 6, 7, 8, 9, 10].join("%2C")
+    return `${baseUrl}?application_id=${API_KEY}&fields=${fields}&tier=${tiers}&page_no=${pageNumber}`
+}
+
+async function fetchPage(pageNumber) {
+    const url = makeUrl(pageNumber)
+    const { body } = await got.get(url, { responseType: "json" })
+    return body
+}
+
+async function fetchAll() {
+    let pageNumber = 1
+    const result = {}
+    while (true) {
+        const { meta, data } = await fetchPage(pageNumber)
+        Object.assign(result, data)
+        if (meta.page_total === pageNumber) {
+            break
+        }
+        pageNumber++
+    }
+    return result
+}
+
+fetchAll().then(console.log)
