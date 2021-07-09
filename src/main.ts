0693a1e1969bae6f32d1f6bc43b8f031800b5b3b
lgfrbcsgo <33672090+lgfrbcsgo@users.noreply.github.com>
2021-07-09 11:00:21 +0200

    Use simplified runtime

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,40 @@
-import { runApp } from "./runtime"
-import { init, update, view } from "./app"
+import { html, render } from "lit-html"
+import { Connection, ConnectionStatus, MoeMessage } from "./connection"
+import { Database } from "./database"
+import { assertNever } from "@practical-fp/union-types"
+import { createRuntime } from "./runtime"
+
+const connection = new Connection()
+const database = new Database()
 
 const root = document.getElementById("root")!
-runApp(init, update, view, root)
+const { action, queueEffect } = createRuntime(undefined, state =>
+    render(html`<h1>Works!</h1>`, root),
+)
+
+const databaseInitialized = action(state => {
+    queueEffect(() => connection.connect())
+    return state
+})
+
+const connectionStatusChanged = action((state, connectionStatus: ConnectionStatus) => {
+    return state
+})
+
+const moeMessageReceived = action((state, message: MoeMessage) => {
+    switch (message.type) {
+        case "MOE_UPDATE":
+            queueEffect(() => database.processUpdate(message))
+            break
+        case "MOE_HISTORY":
+            queueEffect(() => database.processHistory(message))
+            break
+        default:
+            assertNever(message)
+    }
+    return state
+})
+
+connection.messages$.observe(moeMessageReceived)
+connection.status$.observe(connectionStatusChanged)
+database.open().then(databaseInitialized)
--- src/runtime.ts
+++ src/runtime.ts
@@ -1,77 +1,34 @@
-import { render, TemplateResult } from "lit-html"
-import { AnyVariant } from "@practical-fp/union-types"
+export type Renderer<State> = (state: State) => void
+export type Reducer<State, Args extends unknown[]> = (state: State, ...args: Args) => State
+export type Effect<Args extends unknown[]> = (...args: Args) => void
 
-export type Dispatch<Msg extends AnyVariant> = (message: Msg) => void
-export type Command<Msg extends AnyVariant> = (dispatch: Dispatch<Msg>) => void
+export function createRuntime<State>(initialState: State, renderer: Renderer<State>) {
+    let state = initialState
 
-export type UpdateReturn<State, Msg extends AnyVariant> = readonly [state: State, ...commands: Array<Command<Msg>>]
-
-export type Init<State, Msg extends AnyVariant> = () => UpdateReturn<State, Msg>
-export type Update<State, Msg extends AnyVariant> = (state: State, message: Msg) => UpdateReturn<State, Msg>
-export type View<State, Msg extends AnyVariant> = (state: State, dispatch: Dispatch<Msg>) => TemplateResult
-
-export function runApp<State, Msg extends AnyVariant>(
-    init: Init<State, Msg>,
-    update: Update<State, Msg>,
-    view: View<State, Msg>,
-    element: HTMLElement,
-) {
-    const devtools = new Devtools()
-
-    let [state, ...commands] = init()
-
-    const dispatch = (msg: Msg) => {
-        const [nextState, ...commands] = update(state, msg)
-        if (state !== nextState) {
-            state = nextState
-            queueRender(state)
+    const action = <Args extends unknown[]>(reducer: Reducer<State, Args>) => {
+        return (...args: Args) => {
+            const newState = reducer(state, ...args)
+            if (newState !== state) {
+                queueRender()
+            }
+            state = newState
         }
-        devtools.send(msg, state)
-        executeCommands(commands)
     }
 
-    const executeCommands = (commands: Array<Command<Msg>>) => {
-        commands.forEach(command => command(dispatch))
+    const queueEffect = <Args extends unknown[]>(effect: Effect<Args>, ...args: Args) => {
+        queueMicrotask(() => effect(...args))
     }
 
     let animationFrameHandle: number | undefined = undefined
-    const queueRender = (state: State) => {
-        if (animationFrameHandle !== undefined) {
-            cancelAnimationFrame(animationFrameHandle)
-        }
+    const queueRender = () => {
+        if (animationFrameHandle !== undefined) return
         animationFrameHandle = requestAnimationFrame(() => {
-            const viewResult = view(state, dispatch)
-            render(viewResult, element)
+            animationFrameHandle = undefined
+            renderer(state)
         })
     }
 
-    queueRender(state)
-    devtools.init(state)
-    executeCommands(commands)
-}
+    queueRender()
 
-
-class Devtools {
-    private connection: any
-
-    constructor() {
-        this.connection = (window as any).__REDUX_DEVTOOLS_EXTENSION__?.connect({
-            autoPause: true,
-            features: {},
-            maxAge: 25,
-        })
-    }
-
-    init(state: unknown) {
-        queueMicrotask(() => this.connection?.init(state))
-    }
-
-    send(msg: AnyVariant, state: unknown) {
-        queueMicrotask(() =>
-            this.connection?.send({
-                type: msg.tag,
-                payload: msg.value,
-            }, state),
-        )
-    }
+    return { action, queueEffect }
 }
